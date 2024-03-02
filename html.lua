ed96b78a92a31430737ab96dc9dce15f01b878e6
stof-soft <iiapeeego@gmail.com>
2024-03-02 03:08:33 +0100

    M class is created

--- html.lua
+++ html.lua
@@ -1,3 +1,35 @@
-
 local M={}
+
+function M.parse(str)
+    local init_pos=1
+    local length=#str
+    local token_expected='<'
+    local token
+    local token_pos
+    local token_pos_end
+
+
+    while init_pos<=length do
+        token_pos,token_pos_end,token=str:find('[<>]',init_pos)
+        if token then
+            if token==token_expected then
+
+            else
+                --fix invalid tag
+            end
+        else
+
+        end
+    end
+
+end
+
+
+
+
+
+
+
+
+
 return M
